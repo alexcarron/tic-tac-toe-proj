8fa82bcb35ced5f258687a3e2dab737b7ebc6cd2
Alex Carron <thealexcarron@gmail.com>
2022-06-13 11:09:06 -0500

    Add function to display gameboard state

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+let Gameboard = {
+	gameboard: 
+		["X", "X", "O",
+		"O", "X", "X",
+		"X", "O", "O"],		
+}
+
+function displayGameboard() {
+	let gameboard_cells = document.querySelectorAll("div.cell");
+	
+	for (let cell_index in gameboard_cells) {
+		let cell = gameboard_cells[cell_index]
+		let cell_contents = Gameboard.gameboard[cell_index];
+		
+		cell.textContent = cell_contents
+	}
+}
