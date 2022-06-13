f9bd5f439ee59e8c30e4a4464f6fd541e4277872
Alex Carron <thealexcarron@gmail.com>
2022-06-13 11:17:34 -0500

    Add 'addMark' function

--- scripts/script.js
+++ scripts/script.js
@@ -4,16 +4,20 @@ let Gameboard = (function() {
 		"O", "X", "X",
 		"X", "O", "O"]
 	
-	return {gameboard}
-})();
-
-function displayGameboard() {
-	let gameboard_cells = document.querySelectorAll("div.cell");
+	const displayGameboard = function() {
+		let gameboard_cells = document.querySelectorAll("div.cell");
 	
-	for (let cell_index in gameboard_cells) {
-		let cell = gameboard_cells[cell_index]
-		let cell_contents = Gameboard.gameboard[cell_index];
-		
-		cell.textContent = cell_contents
+		for (let cell_index in gameboard_cells) {
+			let cell = gameboard_cells[cell_index]
+			let cell_contents = this.gameboard[cell_index];
+			
+			cell.textContent = cell_contents
+		}
 	}
-}
+	
+	const addMark = function(location, mark) {
+		this.gameboard[location] = mark;
+	}
+		
+	return {gameboard, displayGameboard, addMark}
+})();
